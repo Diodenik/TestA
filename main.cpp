6a1f462d571da0a0e4e9c41433631a0cbf5a6136
Diodenik <artiukhov.nik@gmail.com>
2021-11-11 15:34:10 +0200

    Dynamic array

--- main.cpp
+++ main.cpp
@@ -3,16 +3,45 @@
 
 using namespace std;
 
-int Sum(int a){
-    if(a == 0)
-        return 0;
-    if(a == 1)
-        return 1;
-    qDebug()<<a;
-    return a + Sum(a-1);
-}
-
 int main()
 {
-    qDebug()<<Sum(9); //Hello
+    int row;
+    int col;
+
+    cout<<"Enter q-ty of row"<<endl;
+    cin>>row;
+    cout<<"Enter q-ty of col"<<endl;
+    cin>>col;
+
+    int **arr = new int* [row];
+
+    for(int i = 0; i < row; i++)
+    {
+        arr[i] = new int[col];
+    }
+    ////////////////////////////
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            arr[i][j] = rand() % 50;
+        }
+    }
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            cout<<arr[i][j]<<'\t';
+        }
+        cout<<endl;
+    }
+
+
+    ////////////////////////////
+    for(int i = 0; i < row; i++)
+    {
+        delete [] arr[i];
+    }
+    delete [] arr;
 }
